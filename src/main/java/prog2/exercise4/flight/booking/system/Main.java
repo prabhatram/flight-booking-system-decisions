c61bd145722c31c29bbf4baf154928de1f512194
prabhatram <36849225+prabhatram@users.noreply.github.com>
2023-03-05 16:44:55 +0200

    Main COMMIT

--- src/main/java/prog2/exercise4/flight/booking/system/Main.java
+++ src/main/java/prog2/exercise4/flight/booking/system/Main.java
@@ -1,5 +1,8 @@
 package prog2.exercise4.flight.booking.system;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,39 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Please enter your full name:");
+        String name = reader.nextLine();
+
+        System.out.println("Now, please enter your departure date as YYYY-MM-DD:");
+        String sdepart = reader.nextLine();
+        LocalDate depart = LocalDate.parse(sdepart);
+
+        System.out.println("Now, please enter your return date as YYYY-MM-DD:");
+        String sreturn = reader.nextLine();
+        LocalDate returnDate = LocalDate.parse(sreturn);
+        
+        System.out.println("How many children as passengers. Enter 0 if none: ");
+        int children = reader.nextInt();
+
+        System.out.println("How many adults as passengers: ");
+        int adults = reader.nextInt();
+
+        FlightBooking flightbooking = new FlightBooking(name, depart, returnDate, children, adults);
+
+        flightbooking.setTotalPassengers(children, adults);
+        flightbooking.setFlightID(name);
+        flightbooking.setTicketNumber(name);
+        flightbooking.setDepartureDate(depart);
+        flightbooking.setReturnDate(returnDate);
+        flightbooking.setDepartingTicketPrice(children, adults);
+        flightbooking.setReturnTicketPrice(children, adults);
+        flightbooking.setTotalTicketPrice();
+    
+        System.out.println(flightbooking);
+        
+        reader.close();
     }
 }
